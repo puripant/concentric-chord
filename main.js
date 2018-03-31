4642f1ce08dcc75d31386cdb3d346101a8987b2a
Puripant <puripant@gmail.com>
2018-03-31 23:42:09 +0700

    Drawing a curve by sampling it into straight lines

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ d3.json("all.json", function(error, data) {
   var width = +svg.attr("width");
   var height = +svg.attr("height");
   var outerRadius = Math.min(width, height) * 0.5 - 40;
-  var tangentSize = 50;
+  var tangentSize = outerRadius/4; //half of the distance between outer and innder (outer/2) radii
 
   data.orgs.sort(function(a, b) { return a.type - b.type; });
-  // console.log(data);
 
   var innerAttrs = ["area", "person", "relation", "result", "role", "strategy"];
   var outerAttrs = ["areas", "people", "relations", "results", "roles", "strategies"];
@@ -73,11 +72,6 @@ d3.json("all.json", function(error, data) {
     });
   });
 
-  // console.log(chord1);
-  // console.log(chord1.groups);
-  // console.log(chord2);
-  // console.log(chord2.groups);
-
   var arc = d3.arc()
       .innerRadius(outerRadius/2 - 10)
       .outerRadius(outerRadius/2);
@@ -130,7 +124,6 @@ d3.json("all.json", function(error, data) {
       }
       coords.unshift(coords[coords.length-1])
       coords.splice(-1, 1);
-      // console.log(coords);
 
       var newAStringParts = parts[indices[1]].split(",");
       newAStringParts[newAStringParts.length-3] = 0;
@@ -138,62 +131,44 @@ d3.json("all.json", function(error, data) {
       newAStringParts[newAStringParts.length-1] = coords[2].y;
 
       var radialVectors = [
-        radialVector(coords[0], "out", tangentSize),
-        radialVector(coords[1], "out", tangentSize),
-        radialVector(coords[2], "in", tangentSize),
-        radialVector(coords[3], "in", tangentSize)
+        radialVector(coords[0], "out", tangentSize-10),
+        radialVector(coords[1], "out", tangentSize-10),
+        radialVector(coords[2], "in", tangentSize-10),
+        radialVector(coords[3], "in", tangentSize-10)
       ];
-      var mid = radialAverage(coordAverage(coords[0], coords[1]), coordAverage(coords[2], coords[3]));
-      var mid1 = mid; //nudgeToOrigin(mid, 1, "out"); //radialAverage(coords[1], coords[3]);
-      var mid2 = mid; //nudgeToOrigin(mid, 1, "in"); //radialAverage(coords[0], coords[2]);
-      // var mid1 = {
-      //   x: (coords[1].x+coords[3].x)/2,
-      //   y: (coords[1].y+coords[3].y)/2
-      // }
-      // var mid2 = {
-      //   x: (coords[0].x+coords[2].x)/2,
-      //   y: (coords[0].y+coords[2].y)/2
-      // }
-
-      var parts0 = parts[0].split(",");
-      var parts1 = parts[1].split(",");
-      var coord0 = {
-        x: parts0[0].slice(1),
-        y: parts0[1]
-      }
-      var coord1 = {
-        x: parts1[5],
-        y: parts1[6]
-      }
 
       var angle1 = (radianToOrigin(coords[0]) + radianToOrigin(coords[1]))/2;
       var angle2 = (radianToOrigin(coords[2]) + radianToOrigin(coords[3]))/2;
       var angleDiff = Math.abs((angle2 - angle1) % (2*Math.PI));
 
-      var tangentIn = (radianToOrigin(coord1) - radianToOrigin(coord0)) % (2*Math.PI); //positive for counterclockwise
-      var tangentMid1 = tangentVector(mid1, (tangentIn > 0)? "counterclockwise":"", Math.min(tangentSize*angleDiff/2, tangentSize));
-      var tangentMid2 = tangentVector(mid2, (tangentIn > 0)? "":"counterclockwise", Math.min(tangentSize*angleDiff/2, tangentSize));
+      //sampling a curve by recursively bisectoring it
+      var coordAvg0 = scaleToSize(coordAverage(coords[0], coords[1]), outerRadius/2 + tangentSize);
+      var coordAvg1 = scaleToSize(coordAverage(coords[2], coords[3]), outerRadius/2 + tangentSize);
+      var arcSamplingCoords = [coordAvg0, radialAverage(coordAvg0, coordAvg1), coordAvg1];
+      while(angleDiff > Math.PI/8) {
+        for (var i = 0; i < arcSamplingCoords.length-1; i += 2) {
+          arcSamplingCoords.splice(i+1, 0, radialAverage(arcSamplingCoords[i], arcSamplingCoords[i+1]));
+        }
+        angleDiff /= 2;
+      }
+      arcSamplingCoords.shift();
+      arcSamplingCoords.pop();
+      var arcSamplingNum = arcSamplingCoords.length;
 
-      parts.splice(indices[0]+1, 3,
-        `C${radialVectors[1].x},${radialVectors[1].y},${tangentMid1.x},${tangentMid1.y},${mid1.x},${mid1.y}`,
-        `S${radialVectors[3].x},${radialVectors[3].y},${coords[3].x},${coords[3].y}`,
+      var newSVGstrings = [`Q${radialVectors[1].x},${radialVectors[1].y},${arcSamplingCoords[0].x},${arcSamplingCoords[0].y}`];
+      for (var i = 1; i < arcSamplingNum; i++) {
+        newSVGstrings.push(`L${arcSamplingCoords[i].x},${arcSamplingCoords[i].y}`);
+      }
+      newSVGstrings.push(`Q${radialVectors[3].x},${radialVectors[3].y},${coords[3].x},${coords[3].y}`,
         newAStringParts.join(","),
-        `C${radialVectors[2].x},${radialVectors[2].y},${tangentMid2.x},${tangentMid2.y},${mid2.x},${mid2.y}`,
-        `S${radialVectors[0].x},${radialVectors[0].y},${coords[0].x},${coords[0].y}`);
-        // "Q"+(x1*(0.7+j*0.6))+","+(y1*(0.7+j*0.6))+","+((x1+x2)/2)+","+((y1+y2)/2),
-        // "T"+x2+","+y2);
-        // "C"+(x1*(0.7+j*0.6))+","+(y1*(0.7+j*0.6))+","+((x1+x2)/2)+","+((y1+y2)/2),
-        // "S"+x2+","+y2);
+        `Q${radialVectors[2].x},${radialVectors[2].y},${arcSamplingCoords[arcSamplingNum-1].x},${arcSamplingCoords[arcSamplingNum-1].y}`);
+      for (var i = arcSamplingNum-2; i >= 0; i--) {
+        newSVGstrings.push(`L${arcSamplingCoords[i].x},${arcSamplingCoords[i].y}`);
+      }
+      newSVGstrings.push(`Q${radialVectors[0].x},${radialVectors[0].y},${coords[0].x},${coords[0].y}`);
+
+      parts.splice.apply(parts, [indices[0]+1, 3].concat(newSVGstrings));
     }
-    // console.log(parts);
-    // 0 "M-8.082052866092418,-129.74852762736725"
-    // 1 "A130,130,0,0,1,-2.3880612583373385e-14,-130"
-    // 2 "C-3.3065463576978534e-14,-180,-18.61676810834812,-189.07114467323012,-15.972435706244802,-189.29427044021878"
-    // 3 "S-19.767912721830974,-148.6917268264179,-32.946521203051624,-247.81954471069645"
-    // 4 "A250,250,0,0,0,-35.46434802931588,-247.4717762066122"
-    // 5 "C-21.278608817589525,-148.4830657239673,-13.328103304141486,-189.51739620720744,-15.972435706244802,-189.29427044021878"
-    // 6 "S-11.190534737666425,-179.65180748404697,-8.082052866092418,-129.74852762736725"
-    // 7 "Z"
     return parts.join("");
   }
 
@@ -208,9 +183,6 @@ d3.json("all.json", function(error, data) {
 
   var mouseover = function(index, text, attr) {
     if (!tip_fixed) {
-      // ribbons.classed("highlight", function(path) {
-      //   return path[attr].index == d.index;
-      // });
       ribbons
         .style("fill-opacity", function(path) {
           return (path[attr].index == index)? 0.8:0;
@@ -223,7 +195,6 @@ d3.json("all.json", function(error, data) {
   }
   var mouseout = function(text) {
     if (!tip_fixed) {
-      // ribbons.classed("highlight", false);
       ribbons
         .style("fill-opacity", 0)
         .style("stroke-opacity", 0);
@@ -283,15 +254,13 @@ function distToOrigin(coord) { //relative to origin (0, 0)
 function radianToOrigin(coord) { //relative to origin (0, 0)
   return Math.atan2(coord.y, coord.x); //Math.atan(coord.y/coord.x); //* 180 / Math.PI to convert to degree
 }
-function nudgeToOrigin(coord, unit, direction) { //relative to origin (0, 0)
-  var ratio = unit/distToOrigin(coord);
-  if (direction === "out") {
-    ratio *= -1;
-  }
+function rotate(coord, angle) {
+  var cos = Math.cos(angle);
+  var sin = Math.sin(angle);
   return {
-    x: coord.x*(1-ratio),
-    y: coord.y*(1-ratio)
-  };
+    x: coord.x*cos - coord.y*sin,
+    y: coord.x*sin + coord.y*cos
+  }
 }
 function opposite(coord) {
   return {
@@ -322,12 +291,15 @@ function radialAverage(a, b) {
     y: dist*Math.sin(angle)
   };
 }
-function radialVector(coord, orientation, size) { //vector to or from the origin (0, 0)
+function scaleToSize(coord, size) {
   var dist = distToOrigin(coord);
-  var newCoord = {
+  return {
     x: coord.x*size/dist,
     y: coord.y*size/dist
   };
+}
+function radialVector(coord, orientation, size) { //vector to or from the origin (0, 0)
+  var newCoord = scaleToSize(coord, size);
   if (orientation == "in") {
     newCoord.x *= -1;
     newCoord.y *= -1;
@@ -337,11 +309,10 @@ function radialVector(coord, orientation, size) { //vector to or from the origin
   return newCoord;
 }
 function tangentVector(coord, orientation, size) { //vector tangent to cirlces at the origin (0, 0)
-  var dist = distToOrigin(coord);
-  var newCoord = {
-    x: ((coord.x*coord.y > 0)? -coord.y :  coord.y)*size/dist,
-    y: ((coord.x*coord.y > 0)?  coord.x : -coord.x)*size/dist
-  };
+  var newCoord = scaleToSize({
+    x: ((coord.x*coord.y > 0)? -coord.y :  coord.y),
+    y: ((coord.x*coord.y > 0)?  coord.x : -coord.x)
+  }, size);
   if (orientation == "counterclockwise") { // the default is clockwise
     newCoord.x *= -1;
     newCoord.y *= -1;
